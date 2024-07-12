7a5a9f7a565a4b1d2d59b5cfb0ba248949d24f13
Yasir <99505547+iamzakibb@users.noreply.github.com>
2024-07-12 21:43:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
