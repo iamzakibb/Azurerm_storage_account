c28b61a8f8901acdb9bc0bb4e12af569ec51a096
Yasir <99505547+iamzakibb@users.noreply.github.com>
2024-07-12 21:46:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "location" {
   type        = string
   default     = "West Europe"
 }
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "example-resources"
+}
