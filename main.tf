1e355175250e2cd6fbcdea3ce628f4d1ed96d4a0
Yasir <99505547+iamzakibb@users.noreply.github.com>
2024-07-25 23:31:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,19 +3,8 @@ data "azurerm_resource_group" "example" {
   name = "existing"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacct"
-  resource_group_name      = data.azurerm_resource_group.example.name
-  location                 = data.azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
 
-resource "azurerm_storage_container" "example" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
-}
+
 
 
 //resource "azurerm_resource_group" "example" {
