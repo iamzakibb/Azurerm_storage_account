e2f00c01d4060a91cca01eef153d1faf7896ea4f
Yasir <99505547+iamzakibb@users.noreply.github.com>
2024-07-25 23:40:26 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,21 @@
-provider "azurerm" {
-  features {}
-}
-
 terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.58"
+    }
+  }
+
   backend "azurerm" {
-    resource_group_name   = "example-resources"
-    storage_account_name  = "examplestorageacct"
+    resource_group_name   = "tfstate-rg"
+    storage_account_name  = "tfstatestorageacct"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
+}
+
+provider "azurerm" {
+  features {}
 }
